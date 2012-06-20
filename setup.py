e2ff92ca48dc292c29dce7956be73a38c961db6f
Dan Prince <dprince@redhat.com>
2012-06-20 12:39:51 -0400

    Skip argparse when injecting requirements.
    Also adds some unit tests parse_requirements.
    
    Change-Id: I3d8625d4627c7933d73059a63f96e19f8d9647ab

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ Utilities with minimum-depends for use in setup.py
 import os
 import re
 import subprocess
+import sys
 
 from setuptools.command import sdist
 
@@ -75,6 +76,10 @@ def parse_requirements(requirements_files=['requirements.txt',
                                 line))
         # -f lines are for index locations, and don't get used here
         elif re.match(r'\s*-f\s+', line):
+            pass
+        # argparse is part of the standard library starting with 2.7
+        # adding it to the requirements list screws distro installs
+        elif line == 'argparse' and sys.version_info >= (2, 7):
             pass
         else:
             requirements.append(line)
