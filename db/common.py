dd5dbdab796771384d3d9c6394b8e857de1048fd
Mark McLoughlin <markmc@redhat.com>
2013-02-04 16:32:06 +0000

    Add missing DBDuplicateEntry
    The session.py part of nova's 8835866 was imported, but not the newly
    added exception.
    
    Change-Id: I0d79a8536f0c8f14cb062dc61c5fce100b63fd64

--- db/common.py
+++ db/common.py
@@ -42,3 +42,10 @@ class DBError(Exception):
     def __init__(self, inner_exception=None):
         self.inner_exception = inner_exception
         super(DBError, self).__init__(str(inner_exception))
+
+
+class DBDuplicateEntry(DBError):
+    """Wraps an implementation specific exception."""
+    def __init__(self, columns=[], inner_exception=None):
+        self.columns = columns
+        super(DBDuplicateEntry, self).__init__(inner_exception)
