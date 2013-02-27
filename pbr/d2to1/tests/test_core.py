ea2e44ffc729fb7d5e27b6771f7191e94b9e1c66
Erik M. Bray <embray@stsci.edu>
2013-02-27 15:19:04 -0500

    This is a test that should really work to test this feature

--- pbr/d2to1/tests/test_core.py
+++ pbr/d2to1/tests/test_core.py
@@ -1,3 +1,7 @@
+import glob
+import os
+import tarfile
+
 from . import D2to1TestCase
 
 
@@ -24,3 +28,21 @@ class TestCore(D2to1TestCase):
         self.run_setup('egg_info')
         stdout, _, _ = self.run_setup('--keywords')
         assert stdout == 'packaging,distutils,setuptools'
+
+    def test_sdist_extra_files(self):
+        """
+        Test that the extra files are correctly added.
+        """
+
+        stdout, _, return_code = self.run_setup('sdist', '--formats=gztar')
+
+        # There can be only one
+        try:
+            tf_path = glob.glob(os.path.join('dist', '*.tar.gz'))[0]
+        except IndexError:
+            assert False, 'source dist not found'
+
+        tf = tarfile.open(tf_path)
+        names = ['/'.join(p.split('/')[1:]) for p in tf.getnames()]
+
+        assert 'extra-file.txt' in names
