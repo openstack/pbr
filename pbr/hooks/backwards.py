d09372e59e0a951867da65cd152e7d3f0e8faf0f
Monty Taylor <mordred@inaugust.com>
2013-07-21 09:42:42 -0700

    Set defaults directly in option processing
    Change-Id: Iaa327862fc9e43aade2dd1ed66aa27f335f12ac7

--- pbr/hooks/backwards.py
+++ pbr/hooks/backwards.py
@@ -24,7 +24,6 @@ class BackwardsCompatConfig(base.BaseConfig):
     section = 'backwards_compat'
 
     def hook(self):
-        self.config['include_package_data'] = 'True'
         packaging.append_text_list(
             self.config, 'dependency_links',
             packaging.parse_dependency_links())
