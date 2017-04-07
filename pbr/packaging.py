ac2f1f8dd7716721893d7881ea880ed9621a8d76
Florian Margaine <florian@platform.sh>
2017-04-07 11:48:45 +0200

    Append short commit hash to dev versions.
    Also make sure that the version remains valid. Adding a dot would make
    it impossible to be installed by pip, which just ignores invalid
    versions (e.g. more than 3 dots).
    
    This commit is there for 2 reasons:
    
    1. It matches what the documentation says. For some reason, pbr no
       longer does that.
    2. It lets you have multiple branches with the same number of commits
       away from the latest release that can generate
       packages (e.g. wheels) in the same folder.

--- pbr/packaging.py
+++ pbr/packaging.py
@@ -637,9 +637,10 @@ def _get_version_from_git_target(git_dir, target_version):
             dict(new=new_version, target=target_version))
     if distance == 0:
         return last_semver
-    new_dev = new_version.to_dev(distance)
+    short_sha = git.get_git_short_sha(git_dir)
+    new_dev = new_version.to_dev(distance, short_sha)
     if target_version is not None:
-        target_dev = target_version.to_dev(distance)
+        target_dev = target_version.to_dev(distance, short_sha)
         if target_dev > new_dev:
             return target_dev
     return new_dev
